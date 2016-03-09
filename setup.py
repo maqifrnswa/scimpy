6362f7746bd070e0a381ab8e68a4452c6352ffbc
Scott Howard <showard314@gmail.com>
2016-03-08 21:20:05 -0500

    update setup.py scripts

--- setup.py
+++ setup.py
@@ -6,7 +6,26 @@ except:
 
 setup(
     name = "scimpy",
-    version = "0.0.dev1",
+    version = "0.0.dev0",
+    description ="Scimpy Speaker Design Tool",
+    url = "https://github.com/maqifrnswa/scimpy",
+    author = "Scott Howard",
+    license = "GPLv3+",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+	"Environment :: MacOS X",
+	"Environment :: X11 Applications :: Qt",
+	"Environment :: Win32 (MS Windows)",
+	"Environment :: X11 Applications",
+	"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+	"Natural Language :: English",
+	"Operating System :: MacOS",
+	"Operating System :: Microsoft :: Windows",
+	"Programming Language :: Python :: 3",
+	"Topic :: Artistic Software",
+	"Topic :: Multimedia :: Sound/Audio",
+	"Topic :: Scientific/Engineering :: Physics"],
+    install_requries=["scipy", "numpy", "matplotlib", "pyaudio", "pyside"],
     packages = find_packages(),
     entry_points = {
         'gui_scripts': [
