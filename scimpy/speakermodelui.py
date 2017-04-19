L/A works, changed units, fixes #17

scimpy/speakermodelui.py
@@ -105,13 +105,14 @@ class SealedBoxWidget(QtWidgets.QGroupBox):
             print(wbox, float(self.vbllineedit.text()))
             area_to_length_ratio = (wbox**2) * \
                 (float(self.vbllineedit.text())/1000)/(345**2)
+                # wbox^2*V_b/c^2
             print("A/l in m = ", area_to_length_ratio,
-                  ' for 6" diameter, length inches = ',
-                  (np.pi*(6*0.0254/2)**2/area_to_length_ratio)/0.0254)
-            f3lineedit.setText("TBD")
+                  ' for 4" diameter, length inches = ',
+                  (np.pi*(4*0.0254/2)**2/area_to_length_ratio)/0.0254)
+            f3lineedit.setText("TBD, not implimented")
             qtlabel.setText("TBD")
             self.loveralineedit.setText(
-                "{0:3.0f}".format(1/area_to_length_ratio))
+                "{0:.3g}".format(1/area_to_length_ratio/100))
 
         super(SealedBoxWidget, self).__init__(title)
         layout = QtWidgets.QFormLayout()
@@ -125,13 +126,12 @@ class SealedBoxWidget(QtWidgets.QGroupBox):
         layout.addRow("Vb (ft^3):", vbflineedit)
         qtlabel = QtWidgets.QLabel()
         layout.addRow("Qt:", qtlabel)
-        f3lineedit = QtWidgets.QLineEdit()
+        f3lineedit = QtWidgets.QLabel()  # changed to QLabel from QLineEdit
         f3lineedit.setToolTip("3 dB Cutoff Frequency")
         layout.addRow("f3 (Hz):", f3lineedit)
         self.loveralineedit = QtWidgets.QLineEdit()
         self.loveralineedit.setToolTip("Port/Vent Length to Area Ratio")
-        layout.addRow("L/A (m^-1):",
-                      self.loveralineedit)
+        layout.addRow("L/A (cm^-1):", self.loveralineedit)
 
         resetbtn = QtWidgets.QPushButton("Set to Infinite Baffle")
         resetbtn.clicked.connect(reset_params)
@@ -302,7 +302,7 @@ class SpeakerModelWidget(QtWidgets.QWidget):
                                             )/1000,
                                         l_over_a=float(
                                             sealedboxwidg.loveralineedit
-                                            .text()))
+                                            .text())*100)
 
         def vasflineedit_callback():
             """On Vas in ft^3 change, find and update Cms (which then updates

scimpy/speakertest.py
@@ -107,8 +107,8 @@ increase buffer size."
                                   method='log',
                                   phi=-90)*((2**(8*width))/2.-1)
         data = np.concatenate((np.zeros(int(duration*.1 * datarate)),
-                        data,
-                        np.zeros(int(duration*.1 * datarate))))
+                               data,
+                               np.zeros(int(duration * 0.1 * datarate))))
         if width == 1:
             data = data+(2**(8*width))/2-1
         data = data.astype(dtype=np_type, copy=False)
@@ -137,10 +137,8 @@ increase buffer size."
 
         while self.stream.is_active():
             time.sleep(0.2)
-        
-        print(message)
-        if message != "":
-            self.plotwidget.window().statusbar.showMessage(message, 5000)
+
+        self.plotwidget.window().statusbar.showMessage(message)
 
         input_data = b''.join(input_data)  # [3:]
         input_data = np.fromstring(input_data, dtype=np_type)